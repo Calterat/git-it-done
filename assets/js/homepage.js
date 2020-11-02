1946845c052913b4a26d3636d6b6315f30c0af3d
Calterat <Calterat@gmail.com>
2020-11-02 13:58:47 -0600

    added sorting repos by languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,14 @@
+let userFormEl = document.querySelector("#user-form");
+let nameInputEl = document.querySelector("#username");
+let repoContainerEl = document.querySelector("#repos-container");
+let repoSearchTerm = document.querySelector("#repo-search-term");
+let languageButtonsEl = document.querySelector("#language-buttons");
+
+
+
 const getUserRepos = (user) => {
     // format the github api url
     let apiUrl = `https://api.github.com/users/${user}/repos`;
-
     // make a request to the url
     fetch(apiUrl)
         .then(function(response) {
@@ -19,11 +26,6 @@ const getUserRepos = (user) => {
         });
 }
 
-let userFormEl = document.querySelector("#user-form");
-let nameInputEl = document.querySelector("#username");
-let repoContainerEl = document.querySelector("#repos-container");
-let repoSearchTerm = document.querySelector("#repo-search-term");
-
 const formSubmitHandler = (event) => {
     event.preventDefault();
     let username = nameInputEl.value.trim();
@@ -37,51 +39,65 @@ const formSubmitHandler = (event) => {
 }
 
 const displayRepos = (repos, searchTerm) => {
-
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
-
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl. textContent = "No Repositories found.";
         return;
     }
-
     // loop over repos
     for (i = 0; i < repos.length; ++i) {
         // format repo name
         let repoName = repos[i].owner.login + "/" + repos[i].name;
-
         // create a container for each repo
         let repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
         repoEl.setAttribute("href", `./single-repo.html?repo=${repoName}`);
-
         // create a span element to hold repository name
         let titleEl = document.createElement("span");
         titleEl.textContent = repoName;
-
         // append to container
         repoEl.appendChild(titleEl);
-
         // create a status element
         let statusEl = document.createElement("span");
         statusEl.classList = "flex-row align-center";
-
         // check if current repo has issues or not
         if (repos[i].open_issues_count > 0) {
             statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";       
         } else {
             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
-
         // append to container
         repoEl.appendChild(statusEl);
-
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
     }
 }
 
+const getFeaturedRepos = (language) => {
+    let apiUrl = `https://api.github.com/search/repositories?q=${language}+is:featured&sort=help-wanted-issues`;
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert(`Error: ${response.statusText}`);
+        }
+    });
+}
+
+const buttonClickHandler = (event) => {
+    let language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
