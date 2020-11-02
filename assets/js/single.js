55b42ae3c8dfa065307f6f7a8f2259ca219c88cb
Calterat <Calterat@gmail.com>
2020-11-02 01:59:53 -0600

    completed repo issues page with query parameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,9 @@
 let issueContainerEl = document.querySelector("#issues-container");
 let limitWarningEl = document.querySelector("#limit-warning");
+let repoNameEl = document.querySelector("#repo-name");
 
 const getRepoIssues = (repo) => {
-
     let apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
-
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -18,63 +17,66 @@ const getRepoIssues = (repo) => {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
-
 }
 
 const displayIssues = (issues) => {
-
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
-
     for ( i = 0; i < issues.length; ++i ) {
         // create a link element to take users to the issue on github
         let issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
-
         // create span to hold issue title
         let titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
-
         // append to container
         issueEl.appendChild(titleEl);
-
         // create a type element
         let typeEl = document.createElement("span");
-
         // check if issue is an actual issue or a pull request
         if (issues[i].pull_request) {
             typeEl.textContent = "(Pull Request)";
         } else {
             typeEl.textContent = "(Issue)";
         }
-
         // append to container
         issueEl.appendChild(typeEl);
-
         // append to page
         issueContainerEl.appendChild(issueEl);
     }
-
 }
 
 const displayWarning = (repo) => {
     // add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
-
     // create link element and append
     let linkEl = document.createElement("a");
     linkEl.textContent = "See more Issues on GitHub.com";
     linkEl.setAttribute("href", `https://github.com/${repo}/issues`);
     linkEl.setAttribute("target", "_blank");
-
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+const getRepoName = () => {
+    // grab repo name from url query string
+    let queryString = document.location.search;
+    let repoName = queryString.split("=")[1];    
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);        
+    } else {
+        // if no repo was give, rediract to the homepage
+        document.location.replace("./index.html");
+    }    
+}
+
+getRepoName();
